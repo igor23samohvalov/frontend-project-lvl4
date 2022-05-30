7cafb633195cd4dbabf2d2fdb9f1dd1152fec5e5
igor23samohvalov <sandypandaadv@outlook.com>
2022-05-30 15:36:27 +0300

    no timeouts, no more

--- src/hoc/SocketProvider.jsx
+++ src/hoc/SocketProvider.jsx
@@ -7,10 +7,10 @@ const notify = (phrase, state) => toast[state](phrase, { autoClose: 2000 });
 export const SocketContext = createContext(null);
 
 export function SocketProvider({ children }) {
-  const { t } = useTranslation();
   // eslint-disable-next-line react/jsx-no-constructed-context-values
   const value = {
     newMessage: (socket, message, channelId, setSubmitState) => {
+      console.log(socket.connected)
       socket.emit('newMessage', {
         text: message,
         username: JSON.parse(localStorage.getItem('userId')).username,
@@ -21,30 +21,16 @@ export function SocketProvider({ children }) {
       setDisabled(true);
       socket.timeout(2000).emit('newChannel', {
         name: newChannel,
-      }, (err) => {
-        if (err) {
-          notify(t('networkError'), 'error');
-          setDisabled(false);
-        }
       });
     },
     renameChannel: (socket, { renamedChannel }, id, setDisabled) => {
       socket.timeout(2000).emit('renameChannel', {
         id,
         name: renamedChannel,
-      }, (err) => {
-        if (err) {
-          notify(t('networkError'), 'error');
-          setDisabled(false);
-        }
       });
     },
     removeChannel: (socket, id) => {
-      socket.timeout(2000).emit('removeChannel', { id }, (err) => {
-        if (err) {
-          notify(t('networkError'), 'error');
-        }
-      });
+      socket.timeout(2000).emit('removeChannel', { id });
     },
   };
 
